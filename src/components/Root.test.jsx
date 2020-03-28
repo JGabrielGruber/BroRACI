32f707c74bfb34367b7f5be2858690683ab3c11b
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-03-28 19:38:53 -0300

    Adicionado JEST ao ESlint;

--- src/components/Root.test.jsx
+++ src/components/Root.test.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import Root from './Root.component';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<Root />);
-  const el = getByText(/hello world/i);
-  expect(el).toBeInTheDocument();
+	const { getByText } = render(<Root />);
+	const el = getByText(/hello world/i);
+	expect(el).toBeInTheDocument();
 });
