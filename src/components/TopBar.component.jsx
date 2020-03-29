e5a1cd1434482356b1c51c654100f772cb3fbba6
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-03-29 10:41:39 -0300

    Implementado compente TopBar;

---
+++ src/components/TopBar.component.jsx
@@ -0,0 +1,117 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { makeStyles } from '@material-ui/core/styles';
+import {
+	Toolbar, IconButton, AppBar, Divider, Grid, Typography,
+} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountAvatar from './AccountAvatar.component';
+import OnlineUsers from './OnlineUsers.component';
+import Notification from './Notification.component';
+
+const useStyles = makeStyles(() => ({
+	spacer: {
+		flexGrow: 1,
+	},
+	root: {
+		display: 'flex',
+	},
+}));
+
+export default function TopBar({
+	title,
+	account,
+	users,
+	amountNotifications,
+	stateLogin,
+	onSideMenu,
+	onLogin,
+	onAccount,
+	onNotification,
+	onSearch,
+	isSideMenu,
+}) {
+	const classes = useStyles();
+
+	return (
+		<AppBar color="transparent">
+			<Toolbar>
+				{isSideMenu === true ? (
+					<div />
+				) : (
+					<Grid container alignItems="center" className={classes.root}>
+						<IconButton edge="start" color="inherit" aria-label="menu" onClick={() => onSideMenu()}>
+							<MenuIcon />
+						</IconButton>
+						<Grid item>
+							<Typography variant="h5">
+								{title}
+							</Typography>
+						</Grid>
+					</Grid>
+				)}
+				<Grid container direction="row-reverse">
+					{stateLogin === 'NOT_LOGGED' ? (
+						<AccountAvatar
+							account={
+								{ name: 'Fazer Log-In', image: '', state: 'HAS_IMAGE' }
+							}
+							onClick={() => onLogin()}
+						/>
+					) : (
+						<Grid className={classes.root}>
+							<OnlineUsers users={users} />
+							<Divider orientation="vertical" variant="middle" flexItem />
+							<IconButton onClick={() => onSearch()}>
+								<SearchIcon fontSize="large" />
+							</IconButton>
+							<Notification amount={amountNotifications} onClick={onNotification} />
+							<AccountAvatar
+								account={account}
+								onClick={() => onAccount()}
+							/>
+						</Grid>
+					)}
+				</Grid>
+			</Toolbar>
+		</AppBar>
+	);
+}
+
+TopBar.defaultProps = {
+	title: '',
+	account: {},
+	users: [],
+	amountNotifications: 0,
+	stateLogin: 'NOT_LOGGED',
+	onSideMenu: () => {},
+	onLogin: () => {},
+	onAccount: () => {},
+	onNotification: () => {},
+	onSearch: () => {},
+	isSideMenu: false,
+};
+
+TopBar.propTypes = {
+	title: PropTypes.string,
+	account: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		image: PropTypes.string,
+		state: PropTypes.string,
+	}),
+	users: PropTypes.arrayOf(PropTypes.shape({
+		name: PropTypes.string.isRequired,
+		image: PropTypes.string,
+		state: PropTypes.string,
+	})),
+	amountNotifications: PropTypes.number,
+	stateLogin: PropTypes.string,
+	onSideMenu: PropTypes.func,
+	onLogin: PropTypes.func,
+	onAccount: PropTypes.func,
+	onNotification: PropTypes.func,
+	onSearch: PropTypes.func,
+	isSideMenu: PropTypes.bool,
+};
---
+++ src/stories/TopBar.stories.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { action } from '@storybook/addon-actions';
+
+import TopBar from '../components/TopBar.component';
+
+export default {
+	component: TopBar,
+	title: 'TopBar',
+	excludeStories: /.*Data$/,
+};
+
+export const accountData = {
+	name: 'Testenaldo',
+};
+
+export const usersData = [
+	{
+		name: 'Josivaldo',
+	},
+	{
+		name: 'Oliandro da Cigarra',
+		image: '',
+		state: 'HAS_IMAGE',
+	},
+	{
+		name: 'Kowianha',
+	},
+];
+
+export const actionsData = {
+	onSideMenu: action('onSideMenu'),
+	onLogin: action('onLogin'),
+	onAccount: action('onAccount'),
+	onNotification: action('onNotification'),
+	onSearch: action('onSearch'),
+};
+
+export const Default = () => <TopBar onLogin={actionsData.onLogin} />;
+
+export const LoggedIn = () => (
+	<TopBar
+		title="Testelandia"
+		account={accountData}
+		users={usersData}
+		onSideMenu={actionsData.onSideMenu}
+		onAccount={actionsData.onAccount}
+		onNotification={actionsData.onNotification}
+		stateLogin="LOGGED"
+	/>
+);
+
+export const LoggedInWithNotifications = () => (
+	<TopBar
+		title="Testelandia"
+		account={accountData}
+		users={usersData}
+		amountNotifications={7}
+		onSideMenu={actionsData.onSideMenu}
+		onAccount={actionsData.onAccount}
+		onNotification={actionsData.onNotification}
+		onSearch={actionsData.onSearch}
+		stateLogin="LOGGED"
+	/>
+);
+
+export const LoggedInWithSideMenu = () => (
+	<TopBar
+		title="Testelandia"
+		account={accountData}
+		users={usersData}
+		amountNotifications={7}
+		onSideMenu={actionsData.onSideMenu}
+		onAccount={actionsData.onAccount}
+		onNotification={actionsData.onNotification}
+		onSearch={actionsData.onSearch}
+		stateLogin="LOGGED"
+		isSideMenu
+	/>
+);
