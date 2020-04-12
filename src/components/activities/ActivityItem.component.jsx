65719f040315d3d3beaa77ce4a45d1363c66bee7
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-04-12 17:05:22 -0300

    Criado componente ActivityItem;

---
+++ src/components/activities/ActivityItem.component.jsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+	Grid, Container, Typography, Button,
+} from '@material-ui/core';
+
+import Step from './Step.component';
+import ActivityModel from '../../models/Activity.model';
+import StepModel from '../../models/Step.model';
+import UserGroup from './UserGroup.component';
+import Rule from '../../models/Rule.model';
+
+export default function ActivityItem({
+	activity,
+	steps,
+	rules,
+	onClick,
+	onAddStep,
+	onRemoveStep,
+	onSelectStep,
+	index,
+}) {
+	return (
+		<Container maxWidth="md">
+			<Grid container direction="row" spacing={3} alignItems="center">
+				<Grid item>
+					<Step
+						step={activity.step}
+						steps={steps}
+						onAdd={onAddStep}
+						onRemove={onRemoveStep}
+						onSelect={onSelectStep}
+					/>
+				</Grid>
+				<Grid item xs>
+					<Button style={{ textTransform: 'none' }} fullWidth onClick={() => onClick(activity)}>
+						<Container>
+							<Grid container justify="space-between" alignItems="center">
+								<Grid item>
+									<Typography variant="h5">
+										{activity.title}
+									</Typography>
+								</Grid>
+								<Grid item>
+									<UserGroup assignments={activity.assignments} rules={rules} />
+								</Grid>
+							</Grid>
+						</Container>
+					</Button>
+				</Grid>
+				<Grid item>
+					<Typography variant="body2" color="textSecondary">
+						{`#${index}`}
+					</Typography>
+				</Grid>
+			</Grid>
+		</Container>
+	);
+}
+
+ActivityItem.defaultProps = {
+	index: 1,
+	onAddStep: null,
+	onRemoveStep: null,
+	onSelectStep: null,
+};
+
+ActivityItem.propTypes = {
+	activity: PropTypes.shape(ActivityModel).isRequired,
+	steps: PropTypes.arrayOf(StepModel).isRequired,
+	rules: PropTypes.arrayOf(Rule).isRequired,
+	onClick: PropTypes.func.isRequired,
+	onAddStep: PropTypes.func,
+	onRemoveStep: PropTypes.func,
+	onSelectStep: PropTypes.func,
+	index: PropTypes.number,
+};
---
+++ src/stories/activities/ActivityItem.stories.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { action } from '@storybook/addon-actions';
+
+import UserGroup from '../../components/activities/UserGroup.component';
+import Categories from '../Categories';
+import ActivityItem from '../../components/activities/ActivityItem.component';
+import { assignmentsData, rulesData } from './UserGroup.stories';
+
+export default {
+	component: UserGroup,
+	title: `${Categories.ACTIVITIES}/Activity Item`,
+	excludeStories: /.*Data$/,
+};
+
+export const activityData = {
+	uid: 'eeee',
+	title: 'Criação do esboço',
+	step: {
+		uid: 'bbb',
+		title: 'Esboço',
+	},
+	assignments: assignmentsData,
+};
+
+export const stepsData = [
+	{
+		uid: 'aaa',
+		title: 'Inicio',
+	},
+	{
+		uid: 'bbb',
+		title: 'Esboço',
+	},
+	{
+		uid: 'ccc',
+		title: 'Conclusão',
+	},
+];
+
+export const actionsData = {
+	onClick: action('onClick'),
+	onAddStep: action('onAddStep'),
+	onRemoveStep: action('onRemoveStep'),
+	onSelectStep: action('onSelectStep'),
+};
+
+export const Default = () => (
+	<ActivityItem
+		activity={activityData}
+		steps={stepsData}
+		rules={rulesData}
+		{...actionsData}
+	/>
+);
