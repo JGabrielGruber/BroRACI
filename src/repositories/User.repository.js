e51a011c9fac5214f2248e5f01bec08936589459
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-04-13 11:30:38 -0300

    Criado repositório User;

---
+++ src/repositories/User.repository.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase';
+
+export default {
+	login: async (log) => {
+		try {
+			await firebase.auth().setPersistence(
+				log.remember
+					? firebase.auth.Auth.Persistence.LOCAL
+					: firebase.auth.Auth.Persistence.SESSION,
+			);
+			await firebase.auth().signInWithEmailAndPassword(log.email, log.password);
+			return true;
+		} catch (error) {
+			return false;
+		}
+	},
+	signup: async (sig) => {
+		try {
+			await firebase.auth().createUserWithEmailAndPassword(sig.email, sig.password);
+			await firebase.auth().signInWithEmailAndPassword(sig.email, sig.password);
+			const user = firebase.auth().currentUser;
+			user.updateProfile(sig);
+			return true;
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	},
+	get: async () => firebase.auth().currentUser,
+};
