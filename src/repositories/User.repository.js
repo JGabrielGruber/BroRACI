f027a2982f36fe43cc184f1aadc80b006a65c1da
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-04-13 15:16:11 -0300

    Criado repositório User;

--- src/repositories/User.repository.js
+++ src/repositories/User.repository.js
@@ -1,4 +1,8 @@
 import firebase from 'firebase';
+import { db } from '../configFirebase';
+import User from '../models/User.model';
+
+const collection = 'users';
 
 export default {
 	login: async (log) => {
@@ -18,8 +22,14 @@ export default {
 		try {
 			await firebase.auth().createUserWithEmailAndPassword(sig.email, sig.password);
 			await firebase.auth().signInWithEmailAndPassword(sig.email, sig.password);
+			await firebase.auth().currentUser.updateProfile(sig);
 			const user = firebase.auth().currentUser;
-			user.updateProfile(sig);
+			db.collection(collection).doc(user.uid).set({
+				displayName: user.displayName,
+				photoURL: user.photoURL,
+				email: user.email,
+				phoneNumber: user.phoneNumber,
+			});
 			return true;
 		} catch (error) {
 			console.log(error);
@@ -30,4 +40,11 @@ export default {
 		await firebase.auth().signOut();
 	},
 	get: async () => firebase.auth().currentUser,
+	getByEmail: async (email) => db.collection(collection).where('email', '==', email).get().then((qSnap) => {
+		let u = null;
+		qSnap.forEach((snap) => {
+			u = snap.data();
+		});
+		return u;
+	}),
 };
