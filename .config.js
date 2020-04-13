2875793c66da7808c65359d27c83773ed9157c7b
JGabriel Gruber <jgabrielgruber@hotmail.com>
2020-04-13 09:49:44 -0300

    Adicionado firebase;

---
+++ .config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	apiKey: "",
+	authDomain: "broracci.firebaseapp.com",
+	databaseURL: "https://broracci.firebaseio.com",
+	projectId: "broracci",
+	storageBucket: "broracci.appspot.com",
+	messagingSenderId: "59818664178",
+	appId: "1:59818664178:web:edfb640334e5eeed8b706b"
+ };
